d5a3f61a1e35d4ac64586883bca6212f4997d3e7
mariomine88 <140217568+mariomine88@users.noreply.github.com>
2023-09-09 03:43:54 +0200

    PWA
    PWA text

---
+++ service_worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'my-pwa-cache-v1';
+const CACHE_FILES = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/2048.css',
+  '/2048.js',
+  '/android/',
+  '/ios/'
+];
+
+// Install the service worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(CACHE_FILES);
+    })
+  );
+});
+
+// Activate the service worker and remove old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch assets from cache, if available; otherwise, fetch from network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
